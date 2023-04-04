c1f27803de15d0965974e1c09dd65c880b9d3d5b
Dalton-Kujawa <daltonkujawa@yahoo.com>
2023-04-03 19:15:10 -0500

    Second Commit, Added declaration, constructors, and getters/setters.

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,4 +1,43 @@
 package restaurant;
-
+import java.util.Date;
+import java.util.ArrayList;
 public class Menu {
+    private Date dateUpdated;
+    private ArrayList<MenuItem> items;
+    private MenuItem mostRecent;
+
+//    Constructors
+    public Menu(Date dateUpdated, ArrayList<MenuItem> items, MenuItem mostRecent){
+        this.dateUpdated = dateUpdated;
+        this.items = items;
+        this.mostRecent = mostRecent;
+    }
+
+//    Getters
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
+
+    public MenuItem getMostRecent() {
+        return mostRecent;
+    }
+
+// Setters
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+
+    public void setMostRecent(MenuItem mostRecent) {
+        this.mostRecent = mostRecent;
+    }
+
+
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,4 +1,61 @@
 package restaurant;
+import java.util.Date;
 
 public class MenuItem {
+    private String name;
+    private String description;
+    private String category;
+    private Date dateAdded;
+    private double price;
+
+//    Constructors
+    public MenuItem(String name, String description, String category, Date dateAdded, double price){
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.dateAdded = dateAdded;
+        this.price = price;
+    }
+
+//    Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+//    Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
